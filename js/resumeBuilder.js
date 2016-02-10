4660cd164653aec55ddf0bc124185f905ceb2258
Zheng Xu <zheng.xuca@gmail.com>
2016-02-09 21:44:51 -0800

    jlsjfalsj

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,12 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var formattedName = HTMLheaderName.replace("%data%", "Zheng");
+var formattedRole = HTMLheaderRole.replace("%data%", "Software Engineer");
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
+
+
+
+
